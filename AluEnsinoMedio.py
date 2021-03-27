b9112fb105028a5e835bdd4b2ace87f2bfba4a9c
Kerolenrosa <80648623+Kerolenrosa@users.noreply.github.com>
2021-03-27 20:59:36 -0300

    Add files via upload

---
+++ AluEnsinoMedio.py
@@ -0,0 +1,11 @@
+from Aluno import Aluno
+
+class AluEnsinoMedio(Aluno):
+    def __init__(self, codigo, nome, matricula, ano):
+        Aluno.__init__(self, codigo, nome, matricula)
+        self.ano = ano
+    
+    def imprimir(self):
+        Aluno.imprimir(self)
+        print ("Ano: " , self.ano)
+    

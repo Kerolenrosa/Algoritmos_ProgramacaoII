bbb0d1f7d316ad7991fdabd887d24aa857a23b8f
Kerolenrosa <80648623+Kerolenrosa@users.noreply.github.com>
2021-03-27 20:54:34 -0300

    Add files via upload

---
+++ Aluno.py
@@ -0,0 +1,9 @@
+class Aluno:
+
+    def __init__(self, codigo, nome, matricula):
+        self.codigo = codigo
+        self.nome = nome
+        self.matricula = matricula
+
+    def imprimir (self):
+        print ("Código: ", self.codigo, "\nNome: " , self.nome, "\nMatricula: " , self.matricula)
